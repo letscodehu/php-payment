e187b5b8c083830b570335b2bba3b55597350109
Papp Krisztian <fejlesztes@letscode.hu>
2022-02-18 16:22:47 +0100

    php-payments-2

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<form action="https://www.sandbox.paypal.com/donate" method="post" target="_top">
+<input type="hidden" name="hosted_button_id" value="NEZQ8XGSU2LEU" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_HU/i/scr/pixel.gif" width="1" height="1" />
+</form>
+</html>
